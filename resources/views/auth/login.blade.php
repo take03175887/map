31e202a8bc80d7629dd6c5368b6dbcfffc164d9a
take03175887 <g031t086@s.iwate-pu.ac.jp>
2023-06-13 20:13:56 +0900

    Registerリンクの修正

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,11 @@
 <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
-    
-    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
-        {{ __('Register') }}
-    </a>
-
+    <style>
+        a {
+          margin-left:10px;
+        }
+    </style>
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
@@ -42,6 +42,10 @@
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+                {{ __('Register!') }}
+            </a>
+            
 
             <x-primary-button class="ml-3">
                 {{ __('Log in') }}
