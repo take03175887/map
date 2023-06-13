d0eee8ae2d922c1cf16b10da323e9f2ef20479d7
take03175887 <g031t086@s.iwate-pu.ac.jp>
2023-06-13 19:12:15 +0900

    login画面にRegister追加

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,10 @@
 <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
+    
+    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+        {{ __('Register') }}
+    </a>
 
     <form method="POST" action="{{ route('login') }}">
         @csrf
